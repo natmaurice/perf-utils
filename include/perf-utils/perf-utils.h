0cb9e944e188a75242183bcd590cf17515559624
Nathan Maurice <nathan.maurice@protonmail.com>
2022-10-14 19:46:25 +0200

    Fixed functions without return value

--- include/perf-utils/perf-utils.h
+++ include/perf-utils/perf-utils.h
@@ -18,7 +18,7 @@ struct read_format {
 };
 
 // Source: From perf_event_open manpage
-static long
+static inline int
 perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
                 int cpu, int group_fd, unsigned long flags)
 {
@@ -30,47 +30,47 @@ perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
 }
 
 
-static long
+static inline int
 perf_event_enable(int fd) {
-    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);    
+    return ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);    
 }
 
-static long
+static inline int
 perf_event_enable_all(int fd) {
-    ioctl(fd, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP);
+    return ioctl(fd, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP);
 }
 
 
-static long
+static inline int
 perf_event_disable(int fd) {
-    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+    return ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
 }
 
-static long
+static inline int
 perf_event_disable_all(int fd) {
-    ioctl(fd, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP);
+    return ioctl(fd, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP);
 }
 
-static long 
+static inline int
 perf_event_reset(int fd) {
-    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+    return ioctl(fd, PERF_EVENT_IOC_RESET, 0);
 }
 
-static long
+static inline int
 perf_event_reset_all(int fd) {
-    ioctl(fd, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP);
+    return ioctl(fd, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP);
 }
 
-static long long
+static inline long long
 perf_event_read(int fd) {
     long long count = 0;
     read(fd, &count, sizeof(long long));
     return count;
 }
 
-static long
+static inline void
 perf_event_close(int fd) {
-    close(fd);
+    close(fd);    
 }
 
 
